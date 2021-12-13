8282cfc9dbde9d14df28405a4232d50c550480a5
IgorLiovin105 <igorliovin07@gmail.com>
2021-12-14 00:39:23 +0600

    new commit

--- js/script.js
+++ js/script.js
@@ -8,31 +8,3 @@ if(iconMenu) {
         menuBody.classList.toggle('_active');
     });
 }
-
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-
-if(menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
-
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gatoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
-            
-            if(iconMenu.classList.contains('_active')) {
-                iconMenu.classList.remove('_active')
-                menuBody.classList.remove('_active');
-                document.body.classList.remove('_lock');
-            }
-
-            window.scrollTo ({
-                top: gatoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
